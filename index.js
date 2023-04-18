02ff896becce8614ac893dee9cd5f6c5b6c8413b
anishere <tranbinhan1100@gmail.com>
2023-04-18 18:13:02 +0700

    cap nhat chuc nang edit

--- index.js
+++ index.js
@@ -5,9 +5,33 @@ let list = document.getElementById('list')
 addBtn.addEventListener('click',function() {
     let newItem = document.createElement('li')
     let itemText = document.createElement('span')
+    let editBtn = document.createElement('button')
+    let inputEdit = document.createElement('input')
+    let saveBtn = document.createElement('button')
 
     itemText.innerText = todoInput.value
     newItem.appendChild(itemText)
+
+    editBtn.innerText = 'Edit'
+    inputEdit.style.display = 'none'
+    editBtn.addEventListener('click',function() {
+        inputEdit.value = itemText.innerText
+        itemText.style.display = 'none'
+        inputEdit.style.display = 'inline-block'
+        saveBtn.style.display = 'inline-block'
+    })
+    newItem.appendChild(editBtn)
+    newItem.appendChild(inputEdit)
+
+    saveBtn.innerText = 'Save'
+    saveBtn.style.display = 'none'
+    saveBtn.addEventListener('click',function() {
+        itemText.innerText = inputEdit.value
+        saveBtn.style.display = 'none'
+        inputEdit.style.display = 'none'
+        itemText.style.display = 'inline-block'
+    })
+    newItem.appendChild(saveBtn)
 
     let deleteBtn = document.createElement('button')
     deleteBtn.innerText = 'Delete'
